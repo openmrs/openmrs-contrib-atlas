bfbcd5671a37f74c4f833ff8f1c25db0a7317f5f
alexisduque <alexisd61@gmail.com>
2014-05-17 20:29:49 +0200

    User can edit a new marker

--- app/routes.php
+++ app/routes.php
@@ -13,7 +13,6 @@
 
 Route::get('/', array('as' => 'home', function() 
 {
-
 	if (Session::has(user)) {
 		$user = Session::get(user);
 		Log::info('Logged user: ' . $user->uid);
@@ -35,50 +34,9 @@ Route::get('data.php', array(
 	'before' => 'validateCallback',
 	'uses' => 'DataController@getData'));
 
-Route::filter('secret', function()
-{
-    if ( !Input::has('secret') || !Input::has('id') )
-    {
-        App::abort(400, 'Invalid Content');
-    }
-    if (Input::get('secret') != getenv('PING_DELETE_SECRET'))
-    {
-		App::abort(401, 'Unauthorized');
-		Log::info("Unauthorized attempt to delete ".$deleteId." from ".Request::server('REMOTE_ADDR'))	;
-	}
-});
-
-Route::filter('validateJson', function()
-{
-	$json = json_decode(Request::getContent(), true);
-	if ($json == NULL) App::abort(400, 'Missing data');
-	if (!is_array($json)) App::abort(400, 'Unable to parse data');
-	if (!array_key_exists('id', $json)) App::abort(400, 'Missing id');
-	if (!array_key_exists('geolocation', $json)) App::abort(400, 'Missing geolocation');
-	if (!array_key_exists('name', $json)) App::abort(400, 'Missing name');
-});
-
-Route::filter('validateCallback', function()
-{
-	if (Input::has('callback')) {
-		$callback = Input::get('callback');
-		if (!preg_match('/^[a-z_][a-z0-9_]+$/i', $callback)) App::abort(400, 'Invalid callback.');
-		$reserved = ",abstract,boolean,break,byte,case,catch,char,class,const,continue,default,do,double,else,extends,false,final,finally	float,for,function,goto,if,implements,import,in,instanceof,int,interface,long,native,new,null,package,private,protected	public,return,short,static,super,switch,synchronized,this,throw,throws,transient,true,try,var,void,while,with,";
-		if (strpos($reserved, ",$callback,") !== false) App::abort(400, 'Callback cannot be reserved word.');
-	}
-});
-
 Route::get('auth/multipass/callback', array(
 	'before' => 'multipass',
 	'uses' => 'AuthController@decodeMultipass'));
-
-Route::filter('multipass', function()
-{
-    if ( !Input::has('multipass') || !Input::has('signature') )
-    {
-        App::abort(403, 'Unauthorized action.');
-    }
-});
 
 Route::get('logout', array('as' => 'logout', function()
 {
@@ -86,24 +44,12 @@ Route::get('logout', array('as' => 'logout', function()
     Auth::logout();
     Log::info('User logged out');
     Session::flush();
-    if (App::environment('production')) return Redirect::to('/');
     $url = urlencode(route('home'));
-    return Redirect::to('http://'.$idServer.'/disconnect?destination='. $url);
+    return Redirect::to('https://'.$idServer.'/disconnect?destination='. $url);
 }));
 
 Route::get('login', array('as' => 'login', function()
 {
     $idServer = getenv('ID_HOST');
-    if (App::environment('production')) {
-		//Create User
-    	$user = new \Illuminate\Auth\GenericUser(
-    		array('uid' => 'john.doe', 
-    			'name' => 'John Doe',
-    			'email' => 'john.doe@openmrs.org'));
-    	Log::info('Fake user stored in session: ' . $user->uid);
-    	Auth::login($user);
-    	Session::put('user', $user);;
-    	return Redirect::to('/');
-	}
-    return Redirect::to('http://'.$idServer.'/authenticate/atlas');
+    return Redirect::to('https://'.$idServer.'/authenticate/atlas');
 }));
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,15 +4,17 @@
 <title>OpenMRS Atlas</title>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 <link rel="stylesheet" href="css/atlas.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="css/menu.css" type="text/css" />
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet" />
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <script id="globalnav-script" src="https://id.openmrs.org/globalnav/js/app-optimized.js" type="text/javascript"></script>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/user.js"></script>
 <script type="text/javascript" src="js/atlas.js"></script>
 <script type="text/javascript" src="js/yqlgeo.js"></script>
+<script type="text/javascript" src="js/bootbox.js"></script>
 <script type="text/javascript">
 var map;
 var currentUser;
@@ -47,7 +49,7 @@ setTimeout('initialize()', 500);
   <div id="map_canvas" style="width:100%; height:100%"></div>
   <div id="legend" class="control"></div>
   @if (Session::has(user))
-  <div class="container control logged" id ="login">
+  <div class="atlas-container control logged" id ="login">
       <div class="dropDownControl" id="user"><span class="glyphicon glyphicon-user"></span> {{ $user->name }}</div>
       <div class = "dropDownOptionsDiv" id="logout">
       <div class = "dropDownItemDiv" id="editSite"><img src="images/blue-dot.png">Edit my site</div>
@@ -57,11 +59,11 @@ setTimeout('initialize()', 500);
       </div>          
   </div>
   @else
-    <div class="loginControl dropDownControl control" title="Click to login with your OpenMRS ID" id ="login">
+    <div class="atlas-container loginControl dropDownControl control" title="Click to login with your OpenMRS ID" id ="login">
     <span class="glyphicon glyphicon-log-in"></span> {{ link_to_route('login', 'Login' )}}
   </div>
   @endif
-  <div class="container control login" id ="marker-groups">
+  <div class="atlas-container control login" id ="marker-groups">
       <div class="dropDownControl" id="legendSelected" title="Click to switch legend"></div>
       <div class = "dropDownOptionsDiv" id="legendChoice">
           <div class = "dropDownItemDiv" id="legend1"></div>
@@ -74,5 +76,4 @@ setTimeout('initialize()', 500);
   <input type="hidden" id="user-id" value="{{ $user->uid }} " />
   <input type="hidden" id="user-name" value="{{ $user->name }} " />
   <input type="hidden" id="user-email" value="{{ $user->email }} " />
-</body>
 </html>
