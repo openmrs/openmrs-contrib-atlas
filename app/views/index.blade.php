21eb6864a45bbd6630aa9a704d97999ff3c3b445
alexisduque <alexisd61@gmail.com>
2014-06-25 07:10:00 +0200

    ATLAS-83 : Enable Google Analytics for Atlas

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -56,6 +56,16 @@ $(document).ready(function() {
 
 setTimeout('initialize()', 500);
 </script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function()
+{ (i[r].q=i[r].q||[]).push(arguments)}
+,i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-16695719-3', 'openmrs.org');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
+</script>
 </head>
 <body>
   <div id="map_title"><img src="images/OpenMRS-logo.png" /></div>
