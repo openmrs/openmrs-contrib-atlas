5b0ddb6d55cc6a3b8b8e639f1a77a4bed359ff22
alexisduque <alexisd61@gmail.com>
2014-07-24 12:05:09 +0200

    Include rss generator class in the repo

---
+++ app/lib/rss/bundle.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+  'feeder' => array(
+    'autoloads' => array(
+      'map' => array(
+        'Feed' => '(:bundle)/chained.php',
+        'FeedChannel' => '(:bundle)/feeder.php',
+        'FeedEntry' => '(:bundle)/feeder.php',
+        'Feeder' => '(:bundle)/feeder.php',
+        'TextFeeder' => '(:bundle)/feeder.php',
+        'FeedOut' => '(:bundle)/feeder.php',
+      ),
+    ),
+  ),
+);
