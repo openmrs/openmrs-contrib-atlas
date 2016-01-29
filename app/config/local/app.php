e327981746c5d1bdc79d475082211e563c2f4f6c
skvithalani <saloniv@thoughtworks.com>
2016-01-29 10:52:46 +0530

    [Abhishek/Saloni-ATLAS-119]modified app.php url and added env.local.php

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://openmrsatlas.local',
+	'url' => 'http://localhost/atlas',
 
 	/*
 	|--------------------------------------------------------------------------
---
+++ env.local.php
@@ -0,0 +1,43 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Config File Sample
+|--------------------------------------------------------------------------
+|
+| File should be renamed to .env.production.php or .env.local.php .
+|
+*/
+
+return array(
+
+    /* Database Connection */
+    // data source name
+    'DB_DNS' => 'mysql:host=localhost;dbname=atlas',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'atlas',
+    //mysql server charset  (character_set_server)
+    'DB_CHARSET' => 'latin1',
+    //mysql server collation  (collation_server)
+    'DB_COLLATION' => 'latin1_swedish_ci',
+    // database credentials
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'root',
+
+    //Timezone
+    'TIMEZONE' => 'Asia/Kolkata',
+
+    /* Ping Configuration */
+    // needed to delete an entry
+    'PING_DELETE_SECRET' => 'secret',
+
+    /* API Key for ID Auth */
+    'API_KEY' => '1234567890abcdef',
+    'SITE_KEY' => 'localhost',
+    'ID_HOST' => 'http://localhost:3000',
+
+    /* PhantomJS bin path */
+    'PHANTOM_PATH' => '/usr/bin/phantomjs',
+
+    /* Site URL */
+    'SITE_URL' => 'http://saloni.local/'
+);
