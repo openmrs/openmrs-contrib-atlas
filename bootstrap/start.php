b24bb5fe4937428989b2a931db14eaa06c80c499
skvithalani <saloniv@thoughtworks.com>
2016-02-17 16:57:38 +0530

    [Saloni/Abhishek-ATLAS_120]reverted the value of hostname for local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('localhost'),
+	'local' => array(gethostname()),
 	'prod' => array('atlas-server'),
 	'staged' => array('staged-server'),
 
