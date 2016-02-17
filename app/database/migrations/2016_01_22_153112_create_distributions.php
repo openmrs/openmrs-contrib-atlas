e0cecd6a67e8abd58fdcf7bcf659ef6409a49519
skvithalani <saloniv@thoughtworks.com>
2016-02-17 16:59:11 +0530

    [Saloni/Abhishek-ATLAS-120]changed Distribution model to be compatiblewith mysql version < 5.6

--- app/database/migrations/2016_01_22_153112_create_distributions.php
+++ app/database/migrations/2016_01_22_153112_create_distributions.php
@@ -16,7 +16,6 @@ class CreateDistributions extends Migration {
 			$table->increments('id');
 			$table->string('name', 50);
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
-			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
 			$table->boolean('is_standard')->default(false);
 		});
 
--- app/models/Distribution.php
+++ app/models/Distribution.php
@@ -4,7 +4,7 @@
 class Distribution extends Eloquent
 {
     protected $fillable = array('id', 'name', 'is_standard');
-
+    public $timestamps = false;
 
     public function isNonStandard(){
         return !$this->is_standard;
