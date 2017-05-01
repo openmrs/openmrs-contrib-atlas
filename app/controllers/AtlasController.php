da3902631a5b3a64fd2cd9ac02db327fda161476
Isaac Sears <isaac.j.sears@gmail.com>
2017-05-01 12:30:06 -0400

    Remove 'screenshot' and 'capture' routes (#34)

--- app/controllers/AtlasController.php
+++ app/controllers/AtlasController.php
@@ -5,42 +5,6 @@ class AtlasController extends BaseController {
     /**
      *  Decode multipass token and log user
      */
-
-    public function takeCapture()
-    {
-    	if (!Input::has('legend') || !Input::has('zoom') || !Input::has('lat') || !Input::has('lng'))
-	 		App::abort(500, 'Missing parameters');
-	 	
-    	$phantomjs = getenv('PHANTOM_PATH');
-		$filename = storage_path() . '/captures/capture_' . str_random(12) . '.jpg';
-		$legend = Input::get('legend');
-		$zoom = Input::get('zoom');
-		$lat = Input::get('lat');
-		$lng = Input::get('lng');
-		$siteURL = getenv('SITE_URL');
-	 	Log::info('Temp file name:' . $filename);
-
-	 	App::finish(function($request, $response) use ($filename)
-		{
-			if (file_exists($filename)) {
-				unlink($filename);
-				Log::info('Temp file removed');
-			}
-		});
-		
-	 	$command = $phantomjs . ' ' . public_path() . '/js/capture.js ' . $filename .' '. $legend . ' '
-	 	 . $zoom . ' ' . $lat . ' ' . $lng . ' ' . $siteURL;
-	 	Log::info('Comand:' . $command);
-		shell_exec($command);
-
-    	if ( file_exists($filename)) {
-    		$file = 'atlas_capture.jpg';
-    		return Response::download($filename, $file);
-		} else {	
-    		App::abort(500, 'Capture error');
-		}
-	}
-
 	public function downloadCapture()
     {
 		$legend = Input::get('legend');
@@ -59,29 +23,6 @@ class AtlasController extends BaseController {
 		}
 	}
 
-	public function cronCapture()
-  {
-    $phantomjs = getenv('PHANTOM_PATH');
-		$legend = Input::get('legend');
-		$zoom = Input::get('zoom');
-		$width = Input::get('width');
-		$height = Input::get('height');
-		$size = $width . 'x' . $height;
-		$siteURL = getenv('SITE_URL');
-		$filename =  storage_path(). '/captures/atlas'. $legend . '_' . $size . '.jpg';
-		$path = storage_path(). '/captures';
-	 	$command = $phantomjs . ' ' . public_path() . '/js/capture-cron.js ' . $path .' '. $legend . ' '
-	 	 . $width . ' ' . $height . ' ' . $siteURL . ' > ' . storage_path(). '/phantomjs.log';
-	 	Log::info('Comand:' . $command);
-		shell_exec($command);
-
-    	if ( file_exists($filename)) {
-    		$file = 'atlas_capture.jpg';
-    		return Response::download($filename, $file);
-		} else {	
-    		App::abort(500, 'Capture error');
-		}
-	}
 
 	public function rssGenerator($updates = null)
 	{
