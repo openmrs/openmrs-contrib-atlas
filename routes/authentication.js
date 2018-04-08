7d33b6b36494202e67b0b7aca9916f60d408ef77
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2018-04-08 16:27:09 +1000

    Removing references to ID

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,33 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var Multipass = require('atlas-node-multipass');
-var apikey = '1234567890abcdef';
-var sitekey = 'localhost';
-var multipass = new Multipass(apikey, sitekey);
-
 module.exports = function () {
     //GET login
     router.get('/login', function(req, res, next) {
-
         res.redirect('http://localhost:8080/authenticate/atlas');
     });
 
-    router.get('/auth/multipass/callback', function (req, res) {
-
-        var token = req.query.multipass;
-        var obj = multipass.decode(token);
-        //console.log(obj);
-        req.session.authenticated = true;
-        req.session.user = obj;
-        res.redirect('/');
-        
-    });
-    
     router.get('/logout', function (req, res) {
         req.session.destroy();
         res.redirect('/');
     });
-    
+
     return router;
 };
