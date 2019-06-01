90ea7e891c727dffa5e2a61b35f672b4b2f4e4b4
cintiadr <cintiadrio@gmail.com>
2019-06-01 18:58:07 +1000

    Configure LDAP

--- conf.example.js
+++ conf.example.js
@@ -3,10 +3,10 @@ module.exports = {
     // LDAP Settings
     "server": {
       "uri": process.env.LDAP_URI || "ldap://localhost:3389",
-      "baseDn": "dc=openmrs,dc=org",
-      "rdn": "cn",
-      "loginUser": process.env.LDAP_USER || "admin",
-      "password": process.env.LDAP_PASSWORD || "admin"
+      "baseDn": "ou=system,dc=openmrs,dc=org",
+      "rdn": "uid",
+      "loginUser": process.env.LDAP_USER || "atlas",
+      "password": process.env.LDAP_PASSWORD || "atlas"
     },
     "user": {
       "baseDn": "ou=users,dc=openmrs,dc=org",
