b5d5186d9851c92ab1422af5b8f7431c161df119
HelioStrike <rageofepicfury@gmail.com>
2019-07-12 11:07:45 +0530

    ATLAS-187: Add image to RSS feed

--- routes/rss.js
+++ routes/rss.js
@@ -9,11 +9,13 @@ module.exports = function(connection) {
     const ATLAS_LINK = process.env.ATLAS_RSS_LINK || 'https://atlas.openmrs.org';
     const RSS_LINK = ATLAS_LINK + '/rss';
     const FEED_LENGTH = process.env.ATLAS_RSS_LENGTH || 20;
+    const ATLAS_RSS_IMAGE_URL = process.env.ATLAS_RSS_IMAGE_URL;
+    const IMAGE_RED_DOT = 'https://atlas.openmrs.org/images/red-dot.png';
 
     /* GET RSS Feed */
     router.get('/rss', function(req, res, next) {
 
-        connection.query("SELECT title,description,url,author FROM rss ORDER BY date DESC LIMIT "+FEED_LENGTH, function (error, rows, field) {
+        connection.query("SELECT title,description,url,image_url,author FROM rss ORDER BY date DESC LIMIT "+FEED_LENGTH, function (error, rows, field) {
             if(!!error){
                 console.log(error);
             }
@@ -30,6 +32,17 @@ module.exports = function(connection) {
 
                 rows.forEach(function(row) {
                     row.guid = ATLAS_LINK;
+
+                    var image_url = IMAGE_RED_DOT;
+                    if(row.image_url) {
+                        image_url = row.image_url;
+                    } else if (ATLAS_RSS_IMAGE_URL) {
+                        image_url = ATLAS_RSS_IMAGE_URL;
+                    }
+
+                    row.custom_elements = [
+                        { 'image': image_url }
+                    ]
                     feed.item(row);
                 });
 
